ef24fe690f8ee7b18f1edaae201b88eae251c863
tsukashu <80503344+tsukashu@users.noreply.github.com>
2022-07-06 18:58:42 +0900

    replace audio library end
    make drampad  component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,43 @@ import { Howl, Howler } from 'howler';
 
 const TESTAudio = '/src/assets/drums/Bld_H1.mp3';
 
+
+const DramPad = (props) => {
+  return (
+    <button
+      className='drum-pad'
+      id='audio0'
+      onClick={() => howlerTest(props.audio)}
+    >
+      {props.id}
+      <audio src={props.audio} className='clip' id={props.id}></audio>
+    </button>
+  );
+};
+const DramPadDOM = () => {
+  // this is implement version using Direct DOM manipulation.
+  return (
+    <button
+      className='drum-pad'
+      id='audio0'
+      onClick={() => {
+        document.getElementById('F').play();
+      }}
+    >
+      F
+      <audio src='/src/assets/drums/Bld_H1.mp3' className='clip' id='F'></audio>
+    </button>
+  );
+};
+
+const howlerSoundOnly = () => {
+  const sound = new Howl({
+    src: [TESTAudio],
+  });
+
+  sound.play();
+};
+
 const howlerTest = (audio) => {
   const sound = new Howl({
     src: [audio],
@@ -32,97 +69,19 @@ function App() {
       <div className='App-container' id='drum-machine'>
         <h2>Drum Machine 🥁</h2>
         <div>
-          <p>HowlerTest</p>
-          <button onClick={howlerTest}>HOWLER</button>
           <ReactHotkeyTest />
         </div>
         <div id='display'>display</div>
-        <button
-          className='drum-pad'
-          id='audio1'
-          onClick={() => {
-            document.getElementById('Q').play();
-          }}
-        >
-          Q
-          <audio
-            src='/src/assets/drums/Bld_H1.mp3'
-            className='clip'
-            id='Q'
-          ></audio>
-        </button>
-        <div className='drum-pad' id='audio2'>
-          W
-          <audio
-            controls
-            src='/src/assets/drums/Bld_H1.mp3'
-            className='clip'
-            id='W'
-          ></audio>
-        </div>
-        <div className='drum-pad' id='audio3'>
-          E
-          <audio
-            controls
-            src='/src/assets/drums/Bld_H1.mp3'
-            className='clip'
-            id='E'
-          ></audio>
-        </div>
-        <div className='drum-pad' id='audio4'>
-          A
-          <audio
-            controls
-            src='/src/assets/drums/Bld_H1.mp3'
-            className='clip'
-            id='A'
-          ></audio>
-        </div>
-        <div className='drum-pad' id='audio5'>
-          S
-          <audio
-            controls
-            src='/src/assets/drums/Bld_H1.mp3'
-            className='clip'
-            id='S'
-          ></audio>
-        </div>
-        <div className='drum-pad' id='audio6'>
-          D
-          <audio
-            controls
-            src='/src/assets/drums/Bld_H1.mp3'
-            className='clip'
-            id='D'
-          ></audio>
-        </div>
-        <div className='drum-pad' id='audio7'>
-          Z
-          <audio
-            controls
-            src='/src/assets/drums/Bld_H1.mp3'
-            className='clip'
-            id='Z'
-          ></audio>
-        </div>
-        <div className='drum-pad' id='audio8'>
-          X
-          <audio
-            controls
-            src='/src/assets/drums/Bld_H1.mp3'
-            className='clip'
-            id='X'
-          ></audio>
-        </div>
-        <div className='drum-pad' id='audio9'>
-          C
-          <audio
-            controls
-            src='/src/assets/drums/Bld_H1.mp3'
-            className='clip'
-            id='C'
-          ></audio>
-        </div>
+
+        <DramPad id={'Q'} audio={TESTAudio} />
+        <DramPad id={'W'} audio={TESTAudio} />
+        <DramPad id={'E'} audio={TESTAudio} />
+        <DramPad id={'A'} audio={TESTAudio} />
+        <DramPad id={'S'} audio={TESTAudio} />
+        <DramPad id={'D'} audio={TESTAudio} />
+        <DramPad id={'Z'} audio={TESTAudio} />
+        <DramPad id={'X'} audio={TESTAudio} />
+        <DramPad id={'C'} audio={TESTAudio} />
       </div>
     </div>
   );
